bfde86c885ada2bffc56987bd7ae5d5c19433f19
barney-heatwave <djdise@outlook.com>
2020-10-24 11:44:46 +0100

    Attempt tab play sound on hover over

---
+++ tabsound.js
@@ -0,0 +1,15 @@
+/* old method
+var audio = $("#tabsound")[0];
+$("#tab").mouseenter(function () {
+    audio.play();
+}); */
+
+//Defining variable based on unique ID
+
+var audio = document.getElementById("tabaudio");
+
+//Example of an HTML Audio/Video Method
+
+function playAudio() {
+    audio.play;
+}
